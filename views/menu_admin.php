a19dd47be939a68f4db0c99c87c60b4a05e1603d
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2021-02-12 12:51:35 +0100

    Add menu client/admin views

---
+++ views/menu_admin.php
@@ -0,0 +1,16 @@
+<li class="nav-item">
+    <a class="nav-link" href="/commandes">Liste de mes commandes</a>
+</li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Administration
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="/admin/users/manage">Gestion des utilisateurs</a>
+        <a class="dropdown-item" href="/admin/user/new">Nouvel utilisateur</a>
+        <a class="dropdown-item" href="/admin/items/manage">Gestion des articles</a>
+        <a class="dropdown-item" href="/admin/item/new">Nouvel article</a>
+        <a class="dropdown-item" href="/admin/books/manage">Gestion des commandes</a>
+        <a class="dropdown-item" href="/stat">Statistiques</a>
+    </div>
+</li>
---
+++ views/menu_client.php
@@ -0,0 +1,3 @@
+<li class="nav-item">
+    <a class="nav-link" href="/commandes">Liste de mes commandes</a>
+</li>
