970d6f59e982791afd601b549ac5ee0a476e465e
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2021-02-12 12:45:28 +0100

    require login/password

--- views/login.php
+++ views/login.php
@@ -1,11 +1,11 @@
 <form method="POST">
     <div class="form-group">
         <label for="idlogin">Login</label>
-        <input type="text" class="form-control" id="idlogin" name="login">
+        <input type="text" class="form-control" id="idlogin" name="login" required>
     </div>
     <div class="form-group">
         <label for="idpassword">Password</label>
-        <input type="password" class="form-control" id="idpassword" name="password">
+        <input type="password" class="form-control" id="idpassword" name="password" required>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
