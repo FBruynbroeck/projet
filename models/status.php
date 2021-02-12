0f7b19fb8ed6a757d48b373a8d02961e5cdab331
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2021-02-12 12:37:37 +0100

    Add status model

---
+++ models/status.php
@@ -0,0 +1,17 @@
+<?php
+namespace Projet\Model;
+
+
+class Status extends \Projet\App\Model {
+
+    protected $columns = ['id', 'name'];
+
+    public static function getByName($name){
+        $instance = new static();
+        $query = $instance->_pdo->prepare("select * from ".$instance->table." where name = :name");
+        $query->execute([':name' => $name]);
+        $query->setFetchMode(\PDO::FETCH_CLASS, get_class($instance));
+        return $query->fetch();
+    }
+
+}
