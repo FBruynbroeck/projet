6c44905984973ce6c882e2ed2c4ad9b93e922323
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2021-02-12 12:45:54 +0100

    Add Edit button in user view

--- views/user.php
+++ views/user.php
@@ -1,7 +1,7 @@
 <div>
-    ID: <?php echo $user->id?>
-  <br>
-  Login: <?php echo $user->login?>
-  <br>
-  Email: <?php echo $user->email?>
+    Login: <?php echo $user->login?>
+    <br>
+    Email: <?php echo $user->email?>
+    <br>
+    <a href="/user/<?=$user->login?>/edit" class="btn btn-outline-success">Editer</a>
 </div>
